cabe60dc1133d6bd3896bb0fef6c7e439ffa57f1
twarep <ptwareck@uwaterloo.ca>
2024-07-05 01:34:21 -0400

    created board concept

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,3 +1,37 @@
 export default function Page() {
-  return <p>Board</p>;
+  return (
+    <main className="flex my-2 flex-col items-center justify-between py-6 px-6">
+      <h1 className="font-bold text-4xl mb-4">Board</h1>
+      <div className="grid grid-cols-3 w-full justify-center">
+        <div className="text-3xl font-semibold text-center px-6">To Plan
+          <div className="my-6 flex-col border-2 border-black rounded-md">
+            <p className="text-2xl p-4">Task #4</p>
+          </div>
+          <div className="my-6 flex-col border-2 border-black rounded-md">
+            <p className="text-2xl p-4">Task #5</p>
+          </div>
+        </div>
+        <div className="text-2xl font-semibold text-center px-6">To Do
+          <div className="my-6 flex-col border-2 border-black rounded-md">
+            <p className="text-2xl p-4">Task #2</p>
+          </div>
+          <div className="my-6 flex-col border-2 border-black rounded-md">
+            <p className="text-2xl p-4">Task #3</p>
+          </div>
+        </div>
+        <div className="text-2xl font-semibold text-center px-6">Done
+          <div className="my-6 flex-col border-2 border-black rounded-md">
+            <p className="text-2xl p-4">Task #1</p>
+          </div>
+        </div>
+      </div>
+      <a
+          href="/"
+          className="group rounded-lg border border-black px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="text-xl font-semibold">Complete Sprint{" "}</h2>
+        </a>
+    </main>
+
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex my-2 flex-col items-center justify-between py-6 px-6">
-      <div className="flex flex-row items-stretch ">
-        <p>Balancer</p>
+      <div className="flex flex-row items-stretch mb-2">
+        <h1 className="text-2xl">Balancer</h1>
       </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-rows-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-3xl lg:grid-rows-4 lg:text-left">
         <a
           href="/new"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            New Task{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          <h2 className="mb-3 text-2xl font-semibold">New Task{" "}</h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Create a new task in one of your boards.
+            Create a new task.
           </p>
         </a>
 
@@ -26,14 +19,9 @@ export default function Home() {
           href="/board"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Board{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          <h2 className="mb-3 text-2xl font-semibold">Board{" "}</h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Check your current board for tasks
+            Check your current board for tasks.
           </p>
         </a>
 
@@ -41,12 +29,7 @@ export default function Home() {
           href="/recurring"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Recurring Board{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          <h2 className="mb-3 text-2xl font-semibold">Recurring Board{" "}</h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Check your recurring tasks.
           </p>
@@ -56,12 +39,7 @@ export default function Home() {
           href="/sprints"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Sprints{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          <h2 className="mb-3 text-2xl font-semibold">Sprints{" "}</h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Review your current and past sprints.
           </p>
